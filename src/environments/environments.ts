400f354fa7aaea48c57c53b79c3eca889ee2bf84
Ulrich-stack <cocossouu1@gmail.com>
2025-02-14 09:19:46 +0100

    Removing environments

--- src/environments/environments.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyByKlw9o9AaXJvu_e87htAtjEDc9HFnD7U",
-  authDomain: "flight-tracker-c2a45.firebaseapp.com",
-  projectId: "flight-tracker-c2a45",
-  storageBucket: "flight-tracker-c2a45.firebasestorage.app",
-  messagingSenderId: "92224601522",
-  appId: "1:92224601522:web:8d73a37e6c70a406bf78ab",
-  measurementId: "G-4JZ26JTQ67"
-};
