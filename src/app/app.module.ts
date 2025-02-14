57e089e22a2991cb0cb6092df016bd3469496c93
Ulrich-stack <cocossouu1@gmail.com>
2025-02-14 09:17:12 +0100

    Adding environments.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,10 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { firebaseConfig } from '../environments/environments';
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyByKlw9o9AaXJvu_e87htAtjEDc9HFnD7U",
-  authDomain: "flight-tracker-c2a45.firebaseapp.com",
-  projectId: "flight-tracker-c2a45",
-  storageBucket: "flight-tracker-c2a45.firebasestorage.app",
-  messagingSenderId: "92224601522",
-  appId: "1:92224601522:web:8d73a37e6c70a406bf78ab",
-  measurementId: "G-4JZ26JTQ67"
-};
+
 @NgModule({
   declarations: [
     AppComponent,
